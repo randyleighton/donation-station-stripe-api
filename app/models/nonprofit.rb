772d92f86cdfe9504359854f9950fc94966b27c9
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-23 11:26:56 -0700

    add association non-profit has many donations

--- app/models/nonprofit.rb
+++ app/models/nonprofit.rb
@@ -1,3 +1,5 @@
 class Nonprofit < ActiveRecord::Base
   validates :name, presence: true
+
+  has_many :donations
 end
--- spec/nonprofit_spec.rb
+++ spec/nonprofit_spec.rb
@@ -1,3 +1,5 @@
 describe Nonprofit do
   it { should validate_presence_of :name }
+
+  it { should have_many :donations }
 end
