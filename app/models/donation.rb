db781580e2daf5004441bc90f31a65d3dc40998c
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-23 11:26:03 -0700

    add association donation belongs to non-profit

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,6 @@
 class Donation < ActiveRecord::Base
   validates :amount, presence: true
   validates :token, presence: true
+
+  belongs_to :nonprofit
 end
--- spec/donation_spec.rb
+++ spec/donation_spec.rb
@@ -1,4 +1,6 @@
 describe Donation do
   it { should validate_presence_of :amount }
   it { should validate_presence_of :token }
+
+  it { should belong_to :nonprofit }
 end
