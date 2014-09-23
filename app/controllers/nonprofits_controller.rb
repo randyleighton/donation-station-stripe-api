ce5c81e6494519b96a6fa70d491b19b2b87bb8a0
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-23 10:02:25 -0700

    add CRUD to nonprofit

--- app/controllers/nonprofits_controller.rb
+++ app/controllers/nonprofits_controller.rb
@@ -13,5 +13,40 @@ class NonprofitsController < ApplicationController
     if @nonprofit.valid?
       flash[:notice] = "Your Non-Profit has been added!"
       redirect_to nonprofits_path
+    else
+      flash[:alert] = "Something went wrong. Please try again."
+      render "new"
+    end
+  end
+
+  def show
+    @nonprofit = Nonprofit.find(params[:id])
+  end
+
+  def edit
+    @nonprofit = Nonprofit.find(params[:id])
+  end
+
+  def update
+    @nonprofit = Nonprofit.find(params[:id])
+    if @nonprofit.update(nonprofit_params)
+      flash[:notice] = "Your Non-Profit has been edited."
+      redirect_to nonprofit_path(@nonprofit)
+    else
+      flash[:alert] = "Something went wrong. Please try again."
+      render "edit"
+    end
+  end
+
+  def destroy
+    @nonprofit = Nonprofit.find(params[:id])
+    @nonprofit.destroy
+    flash[:notice] = "Your Non-Profit has been exterminated! EXTERMINATED!"
+    redirect_to nonprofits_path
+  end
+
+private
+  def nonprofit_params
+    params.require(:nonprofit).permit(:name)
   end
 end
