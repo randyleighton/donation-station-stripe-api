b1a23317f4bc8d5242b16579f2c0fe3854360848
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-23 09:56:09 -0700

    add welcome page

--- app/controllers/nonprofits_controller.rb
+++ app/controllers/nonprofits_controller.rb
@@ -1,3 +1,17 @@
 class NonprofitsController < ApplicationController
 
+  def index
+    @nonprofits = Nonprofit.all
+  end
+
+  def new
+    @nonprofit = Nonprofit.new
+  end
+
+  def create
+    @nonprofit = Nonprofit.create(nonprofit_params)
+    if @nonprofit.valid?
+      flash[:notice] = "Your Non-Profit has been added!"
+      redirect_to nonprofits_path
+  end
 end
