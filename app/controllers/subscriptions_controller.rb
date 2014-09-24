420cde99a6845cf7361200d41befca442ddddd64
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-24 14:29:45 -0700

    add plans table

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,35 @@
+class SubscriptionsController < ApplicationController
+    before_filter :authenticate_user!
+
+    def new
+      @nonprofit = Nonprofit.find(params[:nonprofit_id])
+      @subscription = Subscription.new
+    end
+
+    def create
+      @nonprofit = Nonprofit.find(params[:nonprofit_id])
+      @subscription = Subscription.create(subscription_params)
+      if @subscription.valid?
+        flash[:notice] = "Thank you for your subscription!"
+        redirect_to nonprofit_path(@nonprofit)
+      else
+        render "new"
+      end
+    end
+
+    def show
+      @subscription = Subscription.find(params[:id])
+    end
+
+    def destroy
+      @subscription = Subscription.find(params[:id])
+      @subscription.destroy
+      flash[:notice] = "Your subscription has been exterminated! EXTERMINATED!"
+      redirect_to donations_path
+    end
+
+  private
+    def donation_params
+      params.require(:donation).permit(:amount, :token, :nonprofit_id).merge(:user_id => current_user.id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924211715) do
+ActiveRecord::Schema.define(version: 20140924212721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20140924211715) do
 
   create_table "nonprofits", force: true do |t|
     t.string "name"
+  end
+
+  create_table "plans", force: true do |t|
+    t.string  "name"
+    t.integer "amount"
   end
 
   create_table "subscriptions", force: true do |t|
