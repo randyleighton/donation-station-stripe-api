24ce1311d69dad72e4f533142bd4dad986247c39
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-24 12:13:03 -0700

    add association user has many donations

---
+++ db/migrate/20140924190851_add_column_user_id_to_donation_table.rb
@@ -0,0 +1,5 @@
+class AddColumnUserIdToDonationTable < ActiveRecord::Migration
+  def change
+    add_column :donations, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924165228) do
+ActiveRecord::Schema.define(version: 20140924190851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140924165228) do
     t.string   "token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "nonprofits", force: true do |t|
