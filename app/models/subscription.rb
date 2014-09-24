078c449ead3731514b67411342e9d1ebfd67f7f5
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-24 14:33:19 -0700

    add association subscription belongs to plan

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,5 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :nonprofit
+  belongs_to :plan
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,5 @@
 describe Subscription do
   it { should belong_to :user }
   it { should belong_to :nonprofit }
-  # it { should belong_to :plan }
+  it { should belong_to :plan }
 end
