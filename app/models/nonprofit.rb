c62aa12647f3adf3d7d6f3a847060e41e623efab
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-24 14:25:23 -0700

    add association nonprofit has many subscriptions

--- app/models/nonprofit.rb
+++ app/models/nonprofit.rb
@@ -2,4 +2,5 @@ class Nonprofit < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :donations
+  has_many :subscriptions
 end
--- spec/nonprofit_spec.rb
+++ spec/nonprofit_spec.rb
@@ -2,4 +2,6 @@ describe Nonprofit do
   it { should validate_presence_of :name }
 
   it { should have_many :donations }
+  it { should have_many :subscriptions }
+
 end
