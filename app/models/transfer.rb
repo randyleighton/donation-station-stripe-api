b27ed4f4926d195be2673b8aabb18752aed9d218
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-25 13:49:02 -0700

    add methods convert_to_cents and transfer_funds to Transfer

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -2,4 +2,22 @@ class Transfer < ActiveRecord::Base
   validates :amount, presence: true
 
   belongs_to :nonprofit
+  before_save :convert_to_cents
+  before_create :transfer_funds
+
+  def convert_to_cents
+    self.amount = self.amount * 100
+  end
+
+  def transfer_funds
+    Stripe.api_key = ENV["TEST_SECRET"]
+
+    # Create a transfer to the specified recipient
+    transfer = Stripe::Transfer.create(
+      :amount => self.amount
+      :currency => "usd",
+      :recipient => self.nonprofit.id,
+      :card => self.token,
+    )
+  end
 end
