eac0842b7fc2513ec120f91ae5f6577abc343859
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-25 10:56:00 -0700

    add transfer_setup method to Nonprofit model

--- app/models/nonprofit.rb
+++ app/models/nonprofit.rb
@@ -3,4 +3,22 @@ class Nonprofit < ActiveRecord::Base
 
   has_many :donations
   has_many :subscriptions
+
+  def transfer_setup
+    # Set your secret key: remember to change this to your live secret key in production
+    # See your keys here https://dashboard.stripe.com/account
+    Stripe.api_key = ENV["TEST_SECRET"]
+
+    # Get the bank account details submitted by the form
+    token_id = params[:stripeToken]
+
+    # Create a Recipient
+    recipient = Stripe::Recipient.create(
+      :name => self.name,
+      :type => "non-profit",
+      :email => self.email,
+      :bank_account => token_id
+    )
+  end
+
 end
