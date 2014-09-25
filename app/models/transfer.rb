40b782a2be5b16d266fc0ebce57df751f1b35c5b
Randy Leighton & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-25 12:27:25 -0700

    add association transfer belongs to nonprofit

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,3 +1,5 @@
 class Transfer < ActiveRecord::Base
   validates :amount, presence: true
+
+  belongs_to :nonprofit
 end
--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -1,3 +1,4 @@
 describe Transfer do
   it { should validate_presence_of :amount }
+  it { should belong_to :nonprofit }
 end
